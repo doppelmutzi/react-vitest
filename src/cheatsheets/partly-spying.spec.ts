b5dc0ce27064ddd2179296a356e4cf5256537d55
Sebastian Weber <sebastian.weber80@gmail.com>
2024-12-08 21:33:35 +0100

    🎨 Remove descriptions

--- src/cheatsheets/partly-spying.spec.ts
+++ src/cheatsheets/partly-spying.spec.ts
@@ -1,27 +1,27 @@
-import { describe, it, expect, vi } from 'vitest';
-import * as myModule from './myModule';
+import { it, expect, vi } from "vitest";
+import * as myModule from "./myModule";
 
 /* 
 Spy on one export and change implementation, spy on the other import by leaving the implementation intact
 */
-describe('Spy on and change implementation with vi.spyOn', () => {
-  it('should spy on myFunction and change its implementation', () => {
-    const spy = vi.spyOn(myModule, 'anotherFunction');
-    const spyAndMock = vi.spyOn(myModule, 'myFunction').mockImplementation((arg) => {
+it("Spy on and change implementation with vi.spyOn", () => {
+  const spy = vi.spyOn(myModule, "anotherFunction");
+  const spyAndMock = vi
+    .spyOn(myModule, "myFunction")
+    .mockImplementation((arg) => {
       return `spied value: ${arg}`;
     });
 
-    const resultMock = myModule.myFunction('test');
-    const resultSpy = myModule.anotherFunction();
+  const resultMock = myModule.myFunction("test");
+  const resultSpy = myModule.anotherFunction();
 
-    expect(vi.isMockFunction(myModule.myFunction)).toBe(true);
-    expect(vi.isMockFunction(myModule.anotherFunction)).toBe(true);
+  expect(vi.isMockFunction(myModule.myFunction)).toBe(true);
+  expect(vi.isMockFunction(myModule.anotherFunction)).toBe(true);
 
-    expect(resultSpy).toBe('Another original value');
-    expect(spy).toHaveBeenCalledTimes(1);
+  expect(resultSpy).toBe("Another original value");
+  expect(spy).toHaveBeenCalledTimes(1);
 
-    expect(resultMock).toBe('spied value: test');
-    expect(spyAndMock).toHaveBeenCalledTimes(1);
-    expect(spyAndMock).toHaveBeenCalledWith('test');
-  });
+  expect(resultMock).toBe("spied value: test");
+  expect(spyAndMock).toHaveBeenCalledTimes(1);
+  expect(spyAndMock).toHaveBeenCalledWith("test");
 });
